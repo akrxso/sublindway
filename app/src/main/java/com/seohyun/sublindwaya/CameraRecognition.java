681ed92d8481cd8669a755911881c73b95a40a05
akrxso <tjgus991008@gmail.com>
2024-05-07 14:16:06 +0900

    카메라 외부저장소에 저장됨!

--- app/src/main/java/com/seohyun/sublindwaya/CameraRecognition.java
+++ app/src/main/java/com/seohyun/sublindwaya/CameraRecognition.java
@@ -10,6 +10,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -60,34 +62,24 @@ public class CameraRecognition extends AppCompatActivity {
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                Log.e("Camera", "Error occurred while creating the image file", ex);
-                Toast.makeText(this, "Error occurred while creating the image file: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-            if (photoFile != null) {
-                photoURI = FileProvider.getUriForFile(this,
-                        "com.seohyun.sublindwaya.provider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+            // 현재 시간을 기반으로 파일 이름 생성
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+            String fileName = "JPEG_" + timeStamp;
+
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/"); // 'Sublindwaya' 폴더 부분을 제거
+            values.put(MediaStore.Images.Media.TITLE, fileName); // 제목에 파일 이름 설정
+            values.put(MediaStore.Images.Media.DISPLAY_NAME, fileName + ".jpg"); // 실제 파일 이름 설정
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+
+            photoURI = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
         }
     }
 
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-        return image;
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
